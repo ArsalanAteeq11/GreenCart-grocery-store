8d446bf5fd551986f734f68f4bfe99a109184dc9
Arsalan11 <hafizmuhammadarsalan16@gmail.com>
2025-08-08 02:34:59 +0500

    minor fix

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -14,8 +14,9 @@ export const sellerLogin = async (req, res) => {
 
       res.cookie("sellerToken", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+
+        secure: true,
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
       return res.json({
